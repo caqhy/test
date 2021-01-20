f762424f9bc1374dc8e83edec27527ed40319aab
caqhy <1809309636@qq.com>
2021-01-21 00:06:28 +0800

    添加工具类功能

--- bookstore/src/com/caqhy/utils/JdbcUtils.java
+++ bookstore/src/com/caqhy/utils/JdbcUtils.java
@@ -6,7 +6,9 @@ import com.alibaba.druid.pool.DruidDataSourceFactory;
 
 import java.io.InputStream;
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Properties;
 
 public class JdbcUtils {
@@ -22,26 +24,66 @@ public class JdbcUtils {
             e.printStackTrace();
         }
     }
+
+    /**
+     *
+     * @return
+     */
     public static Connection getConnection(){
-
-
-        Connection connection = null;
+        Connection conn = null;
         try {
-            connection = dataSource.getConnection();
+            conn = dataSource.getConnection();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return conn;
+    }
+
+    /**
+     *
+     * @param conn
+     * @param ps
+     * @param rs
+     */
+    public static void closeResource(Connection conn, Statement ps, ResultSet rs){
+        try {
+            if(ps != null)
+                ps.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        try {
+            if(conn != null)
+                conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        try {
+            if(rs != null)
+                rs.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     *
+     * @param conn
+     * @param ps
+     */
+    public static void closeResource(Connection conn, Statement ps){
+        try {
+            if(ps != null)
+                ps.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        try {
+            if(conn != null)
+                conn.close();
         } catch (SQLException e) {
             e.printStackTrace();
         }
 
-        return connection;
-    }
-
-    public static void close(Connection connection){
-        if(connection != null) {
-            try {
-                connection.close();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        }
     }
 }
