6dd3a006f361b29dd1347a4e9ccde5da59d6b73c
caqhy <1809309636@qq.com>
2021-01-20 13:02:22 +0800

    创建User

---
+++ bookstore/src/com/caqhy/pojo/User.java
@@ -0,0 +1,60 @@
+package com.caqhy.pojo;
+
+public class User {
+    private Integer id;
+    private String email;
+    private String username;
+    private String password;
+
+    public User() {
+    }
+
+    public User(Integer id, String email, String username, String password) {
+        this.id = id;
+        this.email = email;
+        this.username = username;
+        this.password = password;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", email='" + email + '\'' +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
